bcc7a9b57b420b30dcf45d25b0bdf2f39ac442f0
izzyrich <s1501956@sms.ed.ac.uk>
2019-02-26 20:05:15 +0000

    Izzy worked on dissertation

--- code/lucas_to_shapefile.R
+++ code/lucas_to_shapefile.R
@@ -1,4 +1,4 @@
-# Script to filter important aspects of LUCAS 2009 dataset for Latvia
+# Script to filter important aspects of LUCAS 2012 dataset for Latvia
 # to only include abandoned land 
 # -------
 
@@ -10,11 +10,16 @@ library(raster)
 library(sf)
 
 # import dataset ----
-lucas <- read_csv("data/2009_lucas.csv")
+lucas <- read_csv("data/2012_lucas.csv")
 
-# filter for abandoned land = U112 "Fallow land" ----
+# filter for abandoned land ----
+U112_options <- c("D10", "D20") # stated that these were abandoned agricultural areas
+list <- c("B", "D", "E") # classes that could be agriculture in U410 (Cropland, shrubland, grassland)
+U410_options <-  paste0("^(", paste(list, collapse = "|"), ")")
+
 lucas_filtered <- lucas %>% 
-  dplyr::filter(LU1 == "U112") %>%
+  dplyr::filter(LU1 == "U112" & "LC1" %in% U112_options | 
+                  LU1 == "U410" & str_detect(LC1, U410_options)) %>%
   dplyr::select(GPS_LAT, GPS_LONG) # potentially need elevation here??
 
 # may need to change column names to change to shapefile
@@ -22,37 +27,46 @@ colnames(lucas_filtered)[colnames(lucas_filtered) == "GPS_LAT"] <- "LAT"
 colnames(lucas_filtered)[colnames(lucas_filtered) == "GPS_LONG"] <- "LONG"
 
 # write to csv ----
-write.csv(lucas_filtered, file = "data/lucas_2009_filtered")
+write.csv(lucas_filtered, file = "data/lucas_2012_filtered.csv")
 
 # write to shp ----
 
 # most successful attempt - I thought this worked but the CRS didn't seem to work
-# and it doesn't work when plotting
 coordinates(lucas_filtered) <- c("LONG", "LAT")
-coords_man <- SpatialPoints(lucas_filtered, 
-                            proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
-lucas_2009_shapefile <- spTransform(coords_man, CRS("+proj=longlat"))
-st_crs(lucas_2009_shapefile)
+this_crs <- st_crs("+proj=longlat +ellps=WGS84 +datum=WGS84")
+sp_file <- st_as_sf(lucas_filtered, coords = c("LAT", "LONG"), crs = this_crs)
+st_crs(sp_file) # no reference system still -- will this be an issue??
 
-# check if it worked - it didn't
+# check if it worked - it did!
 ggplot() +
-  geom_sf(data = lucas_2009_shapefile) +
+  geom_sf(data = sp_file) +
   ggtitle("Map of Plot Locations")
 
-# other trials
-proj4string(lucas_filtered) <- CRS("+proj=longlat +datum=WGS84")
-LLcoor<-spTransform(WGScoor,CRS("+proj=longlat"))
-class(lucas_filtered)
-writeOGR(lucas_filtered, "lucas_2009_shapefile", driver = "ESRI Shapefile")
-locations <- st_as_sf(lucas_filtered, coords = c("GPS_LAT", "GPS_LONG", crs = WGS84))
-st_crs(locations)
+# write to shapefile 
+st_write(sp_file,
+         "data/lucas_2012.shp", driver = "ESRI Shapefile")
 
-df.SP <- st_as_sf(lucas_filtered, coords = c("LONG", "LAT"), crs = 4326)
-st_crs(df.SP)
-df.SP<-st_transform(x = df.SP, crs = 4326)
-df.SP$LONG<-st_coordinates(df.SP)[,1]
-df.SP$LAT<-st_coordinates(df.SP)[,2]
-df.SP<-st_transform(x = df.SP, crs = 4326)
-df.SP$LONG<-st_coordinates(df.SP)[,1] 
-df.SP$LAT<-st_coordinates(df.SP)[,2] 
-writeOGR(df.SP, "lucas_2009_shapefile", driver = "ESRI Shapefile")
+# other trials
+#coordinates(lucas_filtered) <- c("LONG", "LAT")
+#coords_man <- SpatialPoints(lucas_filtered, 
+                            proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84"))
+#lucas_2009_shapefile <- spTransform(coords_man, CRS("+proj=longlat"))
+#st_crs(lucas_2009_shapefile
+#proj4string(lucas_filtered) <- CRS("+proj=longlat +datum=WGS84")
+#LLcoor<-spTransform(WGScoor,CRS("+proj=longlat"))
+#class(lucas_filtered)
+#writeOGR(lucas_filtered, "lucas_2009_shapefile", driver = "ESRI Shapefile")
+#locations <- st_as_sf(lucas_filtered, coords = c("GPS_LAT", "GPS_LONG", crs = WGS84))
+#st_crs(locations)
+#df.SP <- st_as_sf(lucas_filtered, coords = c("LONG", "LAT"), crs = 4326)
+#st_crs(df.SP)
+#df.SP<-st_transform(x = df.SP, crs = 4326)
+#df.SP$LONG<-st_coordinates(df.SP)[,1]
+#df.SP$LAT<-st_coordinates(df.SP)[,2]
+#df.SP<-st_transform(x = df.SP, crs = 4326)
+#df.SP$LONG<-st_coordinates(df.SP)[,1] 
+#df.SP$LAT<-st_coordinates(df.SP)[,2] 
+#writeOGR(df.SP, "lucas_2009_shapefile", driver = "ESRI Shapefile")
+
+
+
