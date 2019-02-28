326d2a077674fd0444607311c663239b38e9cf43
izzyrich <s1501956@sms.ed.ac.uk>
2019-02-28 14:48:45 +0000

    Izzy fixed code

--- code/lucas_to_shapefile.R
+++ code/lucas_to_shapefile.R
@@ -25,21 +25,17 @@ lucas_filtered <- lucas %>%
                  LU1 == "U420" & LC1 == "E30") %>%
   dplyr::select(GPS_LAT, GPS_LONG) # potentially need elevation here??
 
-# may need to change column names to change to shapefile
+# change column names
 colnames(lucas_filtered)[colnames(lucas_filtered) == "GPS_LAT"] <- "LAT"
 colnames(lucas_filtered)[colnames(lucas_filtered) == "GPS_LONG"] <- "LONG"
 
 # write to csv ----
 write.csv(lucas_filtered, file = "data/lucas_2012_filtered.csv")
 
-# most successful attempt - I thought this worked but the CRS didn't seem to work
+# set CRS and transform ----
 coordinates(lucas_filtered) <- c("LONG", "LAT")
-this_crs <- st_crs("+proj=longlat +ellps=WGS84 +datum=WGS84")
-sp_file <- st_as_sf(lucas_filtered, coords = c("LAT", "LONG"), crs = this_crs)
-
-# write to shapefile 
-st_write(sp_file,
-         "data/lucas_2012.shp", driver = "ESRI Shapefile")
+proj4string(lucas_filtered) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +init=epsg:3857")
+points <- spTransform(lucas_filtered, "+proj=longlat +ellps=WGS84 +datum=WGS84 +init=epsg:3857")
 
 # add extent (boundary of Latvia) ----
 latvia <- raster::getData("GADM", country = "LVA", level = 0)
@@ -50,39 +46,14 @@ map <- fortify(latvia)%>%
 colnames(map)[colnames(map) == "lat"] <- "LAT"
 colnames(map)[colnames(map) == "long"] <- "LONG"
 
-# set coordinates 
+# set CRS and transform
 coordinates(map) <- c("LONG", "LAT")
-
-# spatial object (may not be necessary)
-sp_border_file <- st_as_sf(map, coords = c("LAT", "LONG"), crs = this_crs)
-
-# check if it worked - it did!
-ggplot() +
-  geom_sf(data = sp_file) +
-  ggtitle("Map of Plot Locations")
-
-ggplot() +
-  geom_sf(data = sp_file) +
-  geom_sf(data = sp_border_file) +
-  ggtitle("Boundary plot")
-
-# but that doesn't fully set the extent --> change to raster?
-proj4string(lucas_filtered) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +init=epsg:3857")
-points <- spTransform(lucas_filtered, "+proj=longlat +ellps=WGS84 +datum=WGS84 +init=epsg:3857")
-r <- raster(points) # points as raster
-
 proj4string(map) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +init=epsg:3857")
 border_points <- spTransform(map, "+proj=longlat +ellps=WGS84 +datum=WGS84 +init=epsg:3857")
 r_border <- raster(border_points) # border as raster
 
-# set extent 
-setExtent(r, r_border, keepres = FALSE, snap = FALSE)
-shapefile(r, 'file.shp') # doesn't work?
+# set extent ----
+points@bbox <- as.matrix(extent(r_border))
 
-# convert raster back to spatial points dataframe - but still doesn't write to shapefile 
-new_r <- rasterToPoints(r, fun = NULL, spatial = TRUE)
-str(new_r)
-class(new_r)
-writeOGR(new_r, "data/lucas_2012_extent.shp", driver = "ESRI Shapefile")
-st_write(new_r,
-         "data/lucas_2012_extent.shp", driver = "ESRI Shapefile")
+# write to shapefile
+shapefile(points, 'data/2012_lucas.shp')
